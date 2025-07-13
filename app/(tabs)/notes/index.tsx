bf693fcba793a177c73510a93142387c9fbe45ab
Harsh Gaur <hgaur701@gmail.com>
2025-07-14 01:33:25 +0530

    Added useFocusEffect for re-fetching notes data on navigating back to index

--- app/(tabs)/notes/index.tsx
+++ app/(tabs)/notes/index.tsx
@@ -1,11 +1,11 @@
+import Note from "@/components/Note";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import React, { useEffect, useState } from "react";
-import { Pressable, ScrollView, View } from "react-native";
-import { router } from "expo-router";
-import Note from "@/components/Note";
-import styles from "./styles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router, useFocusEffect } from "expo-router";
+import React, { useCallback, useState } from "react";
+import { Pressable, ScrollView, View } from "react-native";
+import styles from "./styles";
 
 interface NoteType {
     _id: string;
@@ -21,25 +21,29 @@ export default function Page() {
 
     const [notes, setNotes] = useState<NoteType[]>([])
 
-    useEffect(() => {
-        async function fetchNotes() {
-            try {
-                const fetchedNotes = await AsyncStorage.getItem('notes');
-                if (fetchedNotes !== null) {
-                    const parsed = JSON.parse(fetchedNotes);
-                    const normalized = parsed.map((n: any) => ({
-                        ...n,
-                        createdAt: new Date(n.createdAt),
-                    }));
-                    setNotes(normalized);
-                }
-            } catch (error) {
-                console.log('failed to load notes: ', error);
+    const fetchNotes = useCallback(async () => {
+        try {
+            const fetchedNotes = await AsyncStorage.getItem('notes');
+            if (fetchedNotes !== null) {
+                const parsed = JSON.parse(fetchedNotes);
+                const normalized = parsed.map((n: any) => ({
+                    ...n,
+                    createdAt: new Date(n.createdAt),
+                }));
+                setNotes(normalized);
+            } else {
+                setNotes([]);
             }
+        } catch (error) {
+            console.log('failed to load notes: ', error);
         }
+    }, []);
 
-        fetchNotes()
-    }, [])
+    useFocusEffect(
+        useCallback(() => {
+            fetchNotes();
+        }, [fetchNotes])
+    );
 
     return (
         <ThemedView style={styles.container}>
